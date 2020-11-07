f74a9e9d0d18336dcb21fafb6de9e4f14a67497d
Oleh T <olezhek_85@list.ru>
2020-11-07 16:35:56 +0200

    Add firebase config

---
+++ config/firebase-config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err.stack);
+  }
+}
+
+const fire = firebase;
+export default fire;
